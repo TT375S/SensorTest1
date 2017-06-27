452ab2d70e026cb8c70bab0e78b943ddcfa0c70c
TT375S <tomataka375@gmail.com>
2017-06-27 10:59:00 +0900

    Add GPS location sensing.

--- app/src/main/java/com/example/user1/sensortest1/MainActivity.java
+++ app/src/main/java/com/example/user1/sensortest1/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.user1.sensortest1;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
 
     private SensorManager sensorManager;
     private TextView textView, textInfo;
@@ -18,6 +31,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     private float sensorZ;
     private boolean flg = true;
 
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -31,6 +45,61 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         // Get an instance of the TextView
         textView = (TextView) findViewById(R.id.text_view);
+
+
+        //位置情報のパーミッションチェック
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        }
+        else{
+            locationStart();
+        }
+    }
+
+    private void locationStart(){
+        Log.d("debug","locationStart()");
+
+        // LocationManager インスタンス生成
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!gpsEnabled) {
+            // GPSを設定するように促す
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+            Log.d("debug", "not gpsEnable, startActivity");
+        } else {
+            Log.d("debug", "gpsEnabled");
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+
+            Log.d("debug", "checkSelfPermission false");
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, this);
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debug","checkSelfPermission true");
+
+                locationStart();
+                return;
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this, "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
     }
 
     @Override
@@ -73,7 +142,6 @@ public class MainActivity extends Activity implements SensorEventListener {
     //このメソッドは、リスナーとして登録してあるどのセンサーの値が変化しても呼ばれる
     @Override
     public void onSensorChanged(SensorEvent event) {
-
         //このメソッドが呼ばれた理由となる、値の変わったセンサのタイプを確かめる必要がある
 
         //重力の影響を除いた加速度センサの場合
@@ -158,14 +226,14 @@ public class MainActivity extends Activity implements SensorEventListener {
 //            vz_rotate = (lowPassZ_rotate - tempZ)/interval;
 
             // High Pass Filter
-            float magRawAX = event.values[0] - lowPassX_rotate;
-            float magRawAY = event.values[1] - lowPassY_rotate;
-            float magRawAZ = event.values[2] - lowPassZ_rotate;
+            float hiPassX_rotate = event.values[0] - lowPassX_rotate;
+            float hiPassY_rotate = event.values[1] - lowPassY_rotate;
+            float hiPassZ_rotate = event.values[2] - lowPassZ_rotate;
 
             //向きの変化速度
-            vx_rotate += magRawAX * interval / 10;
-            vy_rotate += magRawAY * interval / 10;
-            vz_rotate += magRawAZ * interval / 10;
+            vx_rotate += hiPassX_rotate * interval / 10;
+            vy_rotate += hiPassY_rotate * interval / 10;
+            vz_rotate += hiPassZ_rotate * interval / 10;
         }
     }
 
@@ -207,6 +275,36 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                Log.d("debug", "LocationProvider.AVAILABLE");
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                break;
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 }
