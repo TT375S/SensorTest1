6a704282c9155915508f7eed353a25ddc2080c40
TT375S <tomataka375@gmail.com>
2017-06-29 15:46:59 +0900

    センシングサービスの整理中

--- app/src/main/java/com/example/user1/sensortest1/SensingService.java
+++ app/src/main/java/com/example/user1/sensortest1/SensingService.java
@@ -44,12 +44,173 @@ public class SensingService extends Service implements SensorEventListener,
         com.google.android.gms.location.LocationListener,
         HttpGetData.HttpGetDataListner{
 
+    private class MyLocationListener implements GoogleApiClient.ConnectionCallbacks,
+            GoogleApiClient.OnConnectionFailedListener,com.google.android.gms.location.LocationListener
+    {
+
+
+    }
+
+    private class MySensorListener implements SensorEventListener
+    {
+
+        private SensorManager sensorManager;
+        private TextView textView, textInfo;
+        private float sensorX;
+        private float sensorY;
+        private float sensorZ;
+        private boolean flg = true;
+
+        private float speed_rotate = 0; //ジャイロセンサの加速度から計算した回転速度(Javaの命名規約におとなしく従ってキャメルケースにした方がよかった？)
+        private float speed_foward = 0; //GPSから取得した前進速度
+        private float speed_right = 0;  //加速度センサから計算した横移動速度
+        private float currentLatitude = 0;      //現在の位置
+        private float currentLongtitude = 0;
+
+        public void init(){
+            // Get an instance of the SensorManager
+            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+            // Listenerの登録
+            Sensor accel = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); //TYPE_LINEAR_ACCELERATIONは重力の影響を除いた加速度を得る
+            Sensor magne = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);  //ドリフトを考慮したジャイロセンサー値を得る
+            sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(this, magne, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+
+        //-----センサー値取得関係-----
+        final private float k = 0.1f;   //値が大きいほどローパスフィルタの効きが強くなる
+        private float lowPassX = 0,lowPassY = 0,lowPassZ = 0; //ローパスフィルタ通過後の値
+
+        private float rawAx = 0, rawAy=0, rawAz = 0;    //ハイパスフィルタを通した値
+
+        long oldTime = 0;   //前回、センサの値が変更されたとき
+
+        float vx = 0, vy = 0, vz = 0;   //速度だけど端末から見た座標(ローカル座標)なことに注意
+        float x=0, y=0, z=0;    //位置。精度悪くてあてにならない。
+
+        long oldTime_rotate = 0;    //前回更新時刻
+        final private float k_rotate = 0.1f;    //回転センサーの値へのローパスフィルターの効きの強さ
+        private float vx_rotate =0, vy_rotate =0, vz_rotate =0;    //回転センサーの変化量
+        private float lowPassX_rotate =0, lowPassY_rotate =0, lowPassZ_rotate =0;
+
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            //このメソッドが呼ばれた理由となる、値の変わったセンサのタイプを確かめる必要がある
+
+            //重力の影響を除いた加速度センサの場合
+            if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+                //センサーの生の値
+                sensorX = event.values[0];
+                sensorY = event.values[1];
+                sensorZ = event.values[2];
+
+                //LPF
+                lowPassX += (event.values[0] - lowPassX) * k;
+                lowPassY += (event.values[1] - lowPassY) * k;
+                lowPassZ += (event.values[2] - lowPassZ) * k;
+
+                // High Pass Filter
+                rawAx = event.values[0] - lowPassX;
+                rawAy = event.values[1] - lowPassY;
+                rawAz = event.values[2] - lowPassZ;
+
+                String strTmp = "加速度センサー\n"
+                        + " X: " + sensorX + "\n"
+                        + " Y: " + sensorY + "\n"
+                        + " Z: " + sensorZ;
+
+                strTmp += "\nLP加速度センサー\n"
+                        + " X: " + lowPassX + "\n"
+                        + " Y: " + lowPassY + "\n"
+                        + " Z: " + lowPassZ;
+
+                strTmp += "\nLP速度加速度センサー\n"
+                        + " vX: " + vx + "\n"
+                        + " vY: " + vy + "\n"
+                        + " vZ: " + vz;
+                strTmp += "\nLP位置加速度センサー\n"
+                        + " X: " + x + "\n"
+                        + " Y: " + y + "\n"
+                        + " Z: " + z;
+                strTmp += "\nHP磁気センサー\n"
+                        + " X: " + lowPassX_rotate + "\n"
+                        + " Y: " + lowPassY_rotate + "\n"
+                        + " Z: " + lowPassZ_rotate;
+                strTmp += "\nHP磁気センサー速度\n"
+                        + " X: " + vx_rotate + "\n"
+                        + " Y: " + vy_rotate + "\n"
+                        + " Z: " + vz_rotate;
+                textView.setText(strTmp);
+
+                if(flg){
+                    showInfo(event);
+                }
+
+                if(oldTime == 0) oldTime = System.currentTimeMillis();
+                long nowTime = System.currentTimeMillis();
+                long interval = nowTime - oldTime;
+                oldTime = nowTime;
+
+                vx += rawAx * interval / 10; // [cm/s] にする
+                vy += rawAy * interval / 10;
+                vz += rawAz * interval / 10;
+                x += vx * interval / 1000; // [cm] にする
+                y += vy * interval / 1000;
+                z += vz * interval / 1000;
+
+                //スピード更新
+                speed_right = vx;
+                //必然性がないのにここでDODGE判定
+                checkDODGE();
+                //スピードレポート
+                reportSpeed();
+            }
+
+            //回転センサーの場合
+            if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                if(oldTime_rotate == 0) oldTime_rotate = System.currentTimeMillis();
+                long nowTime = System.currentTimeMillis();
+                long interval = nowTime - oldTime_rotate;
+                oldTime_rotate = nowTime;
+
+                float tempX = lowPassX_rotate, tempY = lowPassY_rotate, tempZ = lowPassZ_rotate;
+
+                //LPF
+                lowPassX_rotate += (event.values[0] - lowPassX_rotate) * k_rotate;
+                lowPassY_rotate += (event.values[1] - lowPassY_rotate) * k_rotate;
+                lowPassZ_rotate += (event.values[2] - lowPassZ_rotate) * k_rotate;
+
+                // High Pass Filter
+                float hiPassX_rotate = event.values[0] - lowPassX_rotate;
+                float hiPassY_rotate = event.values[1] - lowPassY_rotate;
+                float hiPassZ_rotate = event.values[2] - lowPassZ_rotate;
+
+                //向きの変化速度
+                vx_rotate += hiPassX_rotate * interval / 10;
+                vy_rotate += hiPassY_rotate * interval / 10;
+                vz_rotate += hiPassZ_rotate * interval / 10;
+
+                //スピード更新
+                speed_rotate = vx;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+
+
+    JSONArray report = new JSONArray();
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        reportSpeed();
-        checkDODGE();
         Log.d("SensingService", "STARTSERVICE!");
-        return super.onStartCommand(intent, flags, startId);
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
     @Override
@@ -63,18 +224,6 @@ public class SensingService extends Service implements SensorEventListener,
         return null;
     }
 
-    private SensorManager sensorManager;
-    private TextView textView, textInfo;
-    private float sensorX;
-    private float sensorY;
-    private float sensorZ;
-    private boolean flg = true;
-
-    private float speed_rotate = 0; //ジャイロセンサの加速度から計算した回転速度(Javaの命名規約におとなしく従ってキャメルケースにした方がよかった？)
-    private float speed_foward = 0; //GPSから取得した前進速度
-    private float speed_right = 0;  //加速度センサから計算した横移動速度
-    private float currentLatitude = 0;      //現在の位置
-    private float currentLongtitude = 0;
 
     private LocationManager locationManager;
 
@@ -94,15 +243,6 @@ public class SensingService extends Service implements SensorEventListener,
     public void onCreate()  {
         Log.d("SensingService", "STARTSERVICE!");
         super.onCreate();
-
-        // Get an instance of the SensorManager
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        // Listenerの登録
-        Sensor accel = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); //TYPE_LINEAR_ACCELERATIONは重力の影響を除いた加速度を得る
-        Sensor magne = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);  //ドリフトを考慮したジャイロセンサー値を得る
-        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
-        sensorManager.registerListener(this, magne, SensorManager.SENSOR_DELAY_FASTEST);
 
         locationStart();
 
@@ -199,160 +339,12 @@ public class SensingService extends Service implements SensorEventListener,
         fusedLocationProviderApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
     }
 
-    //-----センサー値取得関係-----
-    final private float k = 0.1f;   //値が大きいほどローパスフィルタの効きが強くなる
-    private float lowPassX = 0,lowPassY = 0,lowPassZ = 0; //ローパスフィルタ通過後の値
-
-    private float rawAx = 0, rawAy=0, rawAz = 0;    //ハイパスフィルタを通した値
-
-    long oldTime = 0;   //前回、センサの値が変更されたとき
-
-    float vx = 0, vy = 0, vz = 0;   //速度だけど端末から見た座標(ローカル座標)なことに注意
-    float x=0, y=0, z=0;    //位置。精度悪くてあてにならない。
-
-    long oldTime_rotate = 0;    //前回更新時刻
-    final private float k_rotate = 0.1f;    //回転センサーの値へのローパスフィルターの効きの強さ
-    private float vx_rotate =0, vy_rotate =0, vz_rotate =0;    //回転センサーの変化量
-    private float lowPassX_rotate =0, lowPassY_rotate =0, lowPassZ_rotate =0;
 
     //このメソッドは、リスナーとして登録してあるどのセンサーの値が変化しても呼ばれる
     //ので中でどのセンサーか場合分けしなくてはならない
     @Override
     public void onSensorChanged(SensorEvent event) {
-        //このメソッドが呼ばれた理由となる、値の変わったセンサのタイプを確かめる必要がある
 
-        //重力の影響を除いた加速度センサの場合
-        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-            //センサーの生の値
-            sensorX = event.values[0];
-            sensorY = event.values[1];
-            sensorZ = event.values[2];
-
-            //LPF
-            lowPassX += (event.values[0] - lowPassX) * k;
-            lowPassY += (event.values[1] - lowPassY) * k;
-            lowPassZ += (event.values[2] - lowPassZ) * k;
-
-            // High Pass Filter
-            rawAx = event.values[0] - lowPassX;
-            rawAy = event.values[1] - lowPassY;
-            rawAz = event.values[2] - lowPassZ;
-
-            String strTmp = "加速度センサー\n"
-                    + " X: " + sensorX + "\n"
-                    + " Y: " + sensorY + "\n"
-                    + " Z: " + sensorZ;
-
-            strTmp += "\nLP加速度センサー\n"
-                    + " X: " + lowPassX + "\n"
-                    + " Y: " + lowPassY + "\n"
-                    + " Z: " + lowPassZ;
-
-            strTmp += "\nLP速度加速度センサー\n"
-                    + " vX: " + vx + "\n"
-                    + " vY: " + vy + "\n"
-                    + " vZ: " + vz;
-            strTmp += "\nLP位置加速度センサー\n"
-                    + " X: " + x + "\n"
-                    + " Y: " + y + "\n"
-                    + " Z: " + z;
-            strTmp += "\nHP磁気センサー\n"
-                    + " X: " + lowPassX_rotate + "\n"
-                    + " Y: " + lowPassY_rotate + "\n"
-                    + " Z: " + lowPassZ_rotate;
-            strTmp += "\nHP磁気センサー速度\n"
-                    + " X: " + vx_rotate + "\n"
-                    + " Y: " + vy_rotate + "\n"
-                    + " Z: " + vz_rotate;
-            textView.setText(strTmp);
-
-            if(flg){
-                showInfo(event);
-            }
-
-            if(oldTime == 0) oldTime = System.currentTimeMillis();
-            long nowTime = System.currentTimeMillis();
-            long interval = nowTime - oldTime;
-            oldTime = nowTime;
-
-            vx += rawAx * interval / 10; // [cm/s] にする
-            vy += rawAy * interval / 10;
-            vz += rawAz * interval / 10;
-            x += vx * interval / 1000; // [cm] にする
-            y += vy * interval / 1000;
-            z += vz * interval / 1000;
-
-            //スピード更新
-            speed_right = vx;
-            //必然性がないのにここでDODGE判定
-            checkDODGE();
-            //スピードレポート
-            reportSpeed();
-        }
-
-        //回転センサーの場合
-        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            if(oldTime_rotate == 0) oldTime_rotate = System.currentTimeMillis();
-            long nowTime = System.currentTimeMillis();
-            long interval = nowTime - oldTime_rotate;
-            oldTime_rotate = nowTime;
-
-            float tempX = lowPassX_rotate, tempY = lowPassY_rotate, tempZ = lowPassZ_rotate;
-
-            //LPF
-            lowPassX_rotate += (event.values[0] - lowPassX_rotate) * k_rotate;
-            lowPassY_rotate += (event.values[1] - lowPassY_rotate) * k_rotate;
-            lowPassZ_rotate += (event.values[2] - lowPassZ_rotate) * k_rotate;
-
-            // High Pass Filter
-            float hiPassX_rotate = event.values[0] - lowPassX_rotate;
-            float hiPassY_rotate = event.values[1] - lowPassY_rotate;
-            float hiPassZ_rotate = event.values[2] - lowPassZ_rotate;
-
-            //向きの変化速度
-            vx_rotate += hiPassX_rotate * interval / 10;
-            vy_rotate += hiPassY_rotate * interval / 10;
-            vz_rotate += hiPassZ_rotate * interval / 10;
-
-            //スピード更新
-            speed_rotate = vx;
-        }
-    }
-
-    // 加速度センサーの各種情報を表示する
-    private void showInfo(SensorEvent event){
-        String info = "Name: " + event.sensor.getName() + "\n";
-        info += "Vendor: " + event.sensor.getVendor() + "\n";
-        info += "Type: " + event.sensor.getType() + "\n";
-        info += "StringType: " + event.sensor.getStringType()+ "\n";
-
-        int data = event.sensor.getMinDelay();
-        info += "Mindelay: "+String.valueOf(data) +" usec\n";
-
-        data = event.sensor.getMaxDelay();
-        info += "Maxdelay: "+String.valueOf(data) +" usec\n";
-
-        data = event.sensor.getReportingMode();
-        String stinfo = "unknown";
-        if(data == 0){
-            stinfo = "REPORTING_MODE_CONTINUOUS";
-        }else if(data == 1){
-            stinfo = "REPORTING_MODE_ON_CHANGE";
-        }else if(data == 2){
-            stinfo = "REPORTING_MODE_ONE_SHOT";
-        }
-        info += "ReportingMode: "+stinfo +" \n";
-
-        float fData = event.sensor.getMaximumRange();
-        info += "MaxRange: "+String.valueOf(fData) +" \n";
-
-        fData = event.sensor.getResolution();
-        info += "Resolution: "+String.valueOf(fData) +" m/s^2 \n";
-
-        fData = event.sensor.getPower();
-        info += "Power: "+String.valueOf(fData) +" mA\n";
-
-        textInfo.setText(info);
     }
 
     @Override
@@ -447,7 +439,7 @@ public class SensingService extends Service implements SensorEventListener,
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    DataHoldSingleton.getInstance().report.put(jsonObject);
+                    report.put(jsonObject);
                 }
             }
         }
@@ -480,7 +472,7 @@ public class SensingService extends Service implements SensorEventListener,
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        DataHoldSingleton.getInstance().report.put(jsonObject); //レポートに追加
+        report.put(jsonObject); //レポートに追加
     }
 
     @Override
@@ -491,8 +483,8 @@ public class SensingService extends Service implements SensorEventListener,
     public void sendReport(){
         //レポート送信
         HttpPostData post = new HttpPostData();
-        post.execute(DataHoldSingleton.getInstance().report);
+        post.execute(report);
 
-        DataHoldSingleton.getInstance().report = new JSONArray();
+        report = new JSONArray();
     }
 }
